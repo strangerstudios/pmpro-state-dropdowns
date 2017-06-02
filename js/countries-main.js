ca15a874233d615ae3af3e424ee0032b092f848a
Jason Coleman <jason@strangerstudios.com>
2017-06-02 10:04:00 -0400

    Fixing some js enqueue logic. Trying to only load on certain pages. Added support for pmpro-shipping. Needs further testing.

--- js/countries-main.js
+++ js/countries-main.js
@@ -1,14 +1,27 @@
 jQuery(document).ready(function($){
-
+	//for compatibility with older PMPro, make sure bcountry fields have ids
 	jQuery("[name='bcountry']").attr('id', 'bcountry');
+
+	//make sure we have a bcountry field to work with
+	if(jQuery('#bcountry').length) {		
+		jQuery('#bstate').replaceWith('<select id="bstate" name="bstate"></select>');  //convert to dropdown so states can auto-populate   
+
+		populateCountries('bcountry', 'bstate');  
+
+		//load defaults from user meta.
+		jQuery('#bcountry').val(pmpro_state_dropdowns.country_selected);
+		jQuery('#bstate').val(pmpro_state_dropdowns.state_selected);
+	}
 	
-    jQuery('#bstate').replaceWith('<select id="bstate" name="bstate"></select>');  //convert to dropdown so states can auto-populate   
+	//pmpro-shipping support
+	if(jQuery('#scountry').length) {
+		jQuery('#sstate').replaceWith('<select id="sstate" name="sstate"></select>');  //convert to dropdown so states can auto-populate   
 
-    populateCountries('bcountry', 'bstate');  
+		populateCountries('scountry', 'sstate');  
 
-    //load defaults from user meta.
-    jQuery('#bcountry').val(test_object.country_selected);
-    jQuery('#bstate').val(test_object.state_selected);
-
+		//load defaults from user meta.
+		jQuery('#scountry').val(pmpro_state_dropdowns.scountry);
+		jQuery('#sstate').val(pmpro_state_dropdowns.sstate);
+	}
 });
 
