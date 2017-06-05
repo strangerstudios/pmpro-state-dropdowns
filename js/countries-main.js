48f203904793884c2bb9e83f8c706b1c557c9883
Andrew Lima <andrew@arctek.co.za>
2017-06-05 18:35:42 +0200

    Update countries-main.js

--- js/countries-main.js
+++ js/countries-main.js
@@ -7,7 +7,7 @@ jQuery(document).ready(function($){
 	// //make sure we have a bcountry field to work with
 	if(jQuery('#bcountry').length) {	
 		jQuery('#bcountry').attr('class', 'crs-country').attr('data-region-id', 'bstate').attr('data-default-value', pmpro_state_dropdowns.bcountry);	
-		jQuery('#bstate').replaceWith('<select id="bstate" name="bstate"></select>');  //convert to dropdown so states can auto-populate
+		jQuery("[name='bstate']").replaceWith('<select id="bstate" name="bstate"></select>');  //convert to dropdown so states can auto-populate
 		jQuery('#bstate').attr('data-default-value', pmpro_state_dropdowns.bstate );
 
 }	
